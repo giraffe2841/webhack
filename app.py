f1bd1461b40c5611bb3c1a3c5c2d29f46d13ab42
giraffe2841 <giraffe2841@gmail.com>
2026-07-07 08:10:51 +0900

    co

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__, static_folder="static")
 CORS(app)
 
-@app.route("/index.html")
+@app.route("/")
 def page1():
     return send_from_directory("static", "index.html")
 
